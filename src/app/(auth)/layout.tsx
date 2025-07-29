8c7c9771ac265b0f6131c18289b2d955cc88db93
Felipe Macedo <99462868+Zuk3s@users.noreply.github.com>
2025-07-28 22:25:56 -0300

    refactor: simplificar layout de autenticação removendo elementos desnecessários

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,26 +1,9 @@
-import { Clock, GalleryVerticalEnd } from 'lucide-react'
-import Link from 'next/link'
-import type React from 'react'
-
-interface AuthLayoutProps {
-  children: React.ReactNode
-}
-
-export default function AuthLayout({ children }: AuthLayoutProps) {
+export default function AuthLayout({
+  children,
+}: React.AllHTMLAttributes<'main'>) {
   return (
-    <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
-      <div className="flex w-full max-w-sm flex-col gap-6">
-        <Link
-          href="/"
-          className="flex items-center gap-2 self-center font-medium"
-        >
-          <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
-            <GalleryVerticalEnd className="size-4" />
-          </div>
-          TaskFlow.
-        </Link>
-        {children}
-      </div>
-    </div>
+    <main className="flex justify-center items-center w-full min-h-screen">
+      {children}
+    </main>
   )
 }
